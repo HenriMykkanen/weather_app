32c7e93581a26f47c83a5e373459edec6b7c8a7c
HenriMykkanen <144138353+HenriMykkanen@users.noreply.github.com>
2024-01-20 20:05:51 +0200

    Search bar
    Added a search bar inside the appbar of search screen from the package https://pub.dev/packages/app_bar_with_search_switch/example

--- lib/presentation/screens/search_screen.dart
+++ lib/presentation/screens/search_screen.dart
@@ -1,35 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:easy_search_bar/easy_search_bar.dart';
 import 'package:weather_app/application/providers.dart';
+import 'package:weather_app/presentation/displays/current_weather_display.dart';
+import 'package:app_bar_with_search_switch/app_bar_with_search_switch.dart';
 
 class SearchScreen extends ConsumerWidget {
   SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final customCity = ref.watch(customCityProvider);
-    return Scaffold(
-      appBar: EasySearchBar(
-          title: Text('City search'),
-          onSearch: (searchText) {
-            ref.read(customCityProvider.notifier).state = searchText;
-          }),
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBarWithSearchSwitch(
+        onSubmitted: (text) {
+          ref.read(customCityProvider.notifier).state = text;
+        },
+        animation: AppBarAnimationSlideLeft.call,
+        appBarBuilder: (context) {
+          return AppBar(
+            title: Text(('City search')),
+            actions: const [
+              AppBarSearchButton(),
+            ],
+          );
+        },
+      ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Center(
           child: Column(children: [
-            const SizedBox(
-              width: 48,
-              height: 96,
-            ),
-            Text(
-              'custom city is ${customCity}',
-              style: Theme.of(context).textTheme.displayLarge,
-            ),
+            Expanded(
+                flex: 3,
+                child: CurrentWeatherDisplay(cityProvider: customCityProvider))
           ]),
         ),
       ),
-    );
+    ));
   }
 }
